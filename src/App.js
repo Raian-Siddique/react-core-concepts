1b71fce397d025b567becac797befe2b9d4be921
Raian-Siddique <raian0197@gmail.com>
2020-08-23 14:37:11 +0600

    added one dynamic page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var person = {
+    name: "Dr. Mahfuz",
+    job: "Singer",
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>i love react</p>
+        <h1>heading {person.name + " " + person.job + (3 + 2)}</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
